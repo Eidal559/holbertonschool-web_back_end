ff85d20068f8a58e3765a3c091bdeeea808df95d
Eidal559 <eidaljuarbe@gmail.com>
2024-05-10 10:46:16 -0400

    task-7

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,3 +1,8 @@
 export default function getBudgetObject(income, gdp, capita) {
-  return { income, gdp, capita };
+  const budget = {
+    income: income,
+    gdp: gdp,
+    capita: capita
+  };
+    return budget;
 }
