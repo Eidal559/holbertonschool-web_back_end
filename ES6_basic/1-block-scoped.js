6b4f3de8d8edd44a468196e15be325408116ee07
Eidal559 <eidaljuarbe@gmail.com>
2024-05-10 10:57:56 -0400

    task-1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,8 +1,13 @@
 export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = true;
+    task2 = false;
   }
+
+  console.log(task, task2);
 
   return [task, task2];
 }
