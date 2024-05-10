b58cbb2c6573e02c3385a8dc500dcc700fe0982b
Eidal559 <eidaljuarbe@gmail.com>
2024-05-10 11:34:24 -0400

    task-1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,3 @@
-export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
-  }
-
-  console.log(task, task2);
+export default function getBudgetObject(income, gdp, capita) {
+  return { income, gdp, capita };
 }
