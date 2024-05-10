61111fd301338b554510c40bc17d15f777bc66e0
Eidal559 <eidaljuarbe@gmail.com>
2024-05-10 09:08:23 -0400

    task-5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,16 +2,16 @@ export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building
     && this.evacuationWarningMessage === undefined) {
-    throw Error('Class extending Building must override evacuationWarningMessage');
-  }
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
-    set sqft(sqft) {
-      this._sqft = sqft;
-    }
-
-    get sqft() {
-      return this._sqft;
-    }
+  set sqft(sqft) {
+    this._sqft = sqft;
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
