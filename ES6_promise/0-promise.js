9e7ac09ee6acb9b8d36f3d44114b7423519dde2b
Eidal559 <eidaljuarbe@gmail.com>
2024-05-14 12:12:33 -0400

    task-0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,15 +1,12 @@
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      // Simulate API call or any asynchronous operation
-      setTimeout(() => {
-        // Simulate a successful response
-        const response = "This is the response from the API";
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const response = "This is the response from the API";
 
-        // Resolve the promise with the response
-        resolve(response);
+      resolve(response);
 
-      }, 2000); // Simulated delay of 2 seconds
-    });
-  }
+    }, 2000);
+  });
+}
 
   export default getResponseFromAPI;
