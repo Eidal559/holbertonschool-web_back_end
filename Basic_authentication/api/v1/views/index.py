3f65cc037ddd7be71493ba8dfc8dd1777e324ec7
Eidal559 <eidaljuarbe@gmail.com>
2024-10-05 22:06:12 -0400

    task fix

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ 
+    """
     GET /api/v1/status
     Return:
       - the status of the API
@@ -16,7 +16,7 @@ def status() -> str:
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ 
+    """
     GET /api/v1/stats
     Return:
       - the number of each object
@@ -29,7 +29,7 @@ def stats() -> str:
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def test_forbidden() -> str:
-    """ 
+    """
     GET /api/v1/forbidden
     Return:
       - Raise error 403 Forbidden
@@ -39,7 +39,7 @@ def test_forbidden() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def test_unauthorized() -> str:
-    """ 
+    """
     GET /api/v1/unauthorized
     Return:
       - Raise error 401 Unauthorized
