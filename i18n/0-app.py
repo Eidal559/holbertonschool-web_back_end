8fc5805b4f3aeccb38444f3e5d5043388d74087f
Eidal559 <eidaljuarbe@gmail.com>
2024-10-24 19:55:47 -0400

    updated task

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+""" Route module for the API - Basic Flask app """
 
-app = Flask(__name__)
 
-@app.route('/')
+from flask import Flask, request, render_template
+from os import getenv
+
+app = Flask(__name__, static_url_path='')
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
+    """ GET /
+        Return: 0-index.html
+    """
     return render_template('0-index.html')
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
