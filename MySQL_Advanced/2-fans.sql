a6862993255ae1de70a34e5a96e3260813f7f202
Ladie juarbe <139393459+Eidal559@users.noreply.github.com>
2024-11-09 10:46:40 -0400

    Delete MySQL_Advanced directory

--- MySQL_Advanced/2-fans.sql
+++
@@ -1,5 +0,0 @@
--- Rank countries by the number of fans
-SELECT origin, SUM(fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
--- MySQL_Advanced/3-glam_rock.sql
+++
@@ -1,6 +0,0 @@
--- List Glam rock bands ranked by their longevity
-SELECT band_name,
-       IFNULL(split - formed, YEAR(CURDATE()) - formed) AS lifespan
-FROM metal_bands
-WHERE style = 'Glam rock'
-ORDER BY lifespan DESC;
