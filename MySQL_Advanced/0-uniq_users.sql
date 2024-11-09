749c2ebd67540b7ed1acaed7043f41f9001fd553
Eidal559 <eidaljuarbe@gmail.com>
2024-11-09 09:50:29 -0400

    fix

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,15 +1,6 @@
-DROP TABLE IF EXISTS users;
-
+-- Create table users with unique email constraint
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL
 );
-
-INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Bob");
-INSERT INTO users (email, name) VALUES ("sylvie@dylan.com", "Sylvie");
-
--- This insert should fail because of the unique email constraint
-INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Jean");
-
-SELECT * FROM users;
